a3418be0cb0afdfcb7a8d07e5d357c5a8dadd5b1
Kamil Dziedzic <arvenil@klecza.pl>
2012-08-20 00:14:42 +0200

    cleanup

--- src/Lock/MySqlLock.php
+++ src/Lock/MySqlLock.php
@@ -24,8 +24,6 @@ class MySqlLock extends LockAbstract {
      */
     protected $pdo;
 
-    protected $lock = 'ble';
-
     public function __construct(\PDO $pdo) {
         parent::__construct();
         
