6e5d262caa85a6dbf1220b587cc909cbedf9a083
Kamil Dziedzic <arvenil@klecza.pl>
2012-08-20 09:51:17 +0200

    s/Mutex/ninja-mutex/

--- src/Lock/MySqlLock.php
+++ src/Lock/MySqlLock.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Mutex.
+ * This file is part of ninja-mutex.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
