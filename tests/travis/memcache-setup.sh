c2ecac686c0fa13d974aba072f373749744ca3da
Kamil Dziedzic <arvenil@klecza.pl>
2013-02-18 22:49:05 +0100

    Try to refactor

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-VERSION="2.2.7"
+install_memcache() {
+    if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
+        echo 'y' | pecl install memcache
+    else
+        MEMCACHE_VERSION="2.2.7"
+        wget "http://pecl.php.net/get/memcache-$MEMCACHE_VERSION.tgz" &&
+        tar -zxf "memcache-$MEMCACHE_VERSION.tgz" &&
+        sh -c "cd memcache-$MEMCACHE_VERSION && phpize && ./configure --enable-memcache && make && sudo make install" &&
+        echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+    fi
 
-if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
-    echo 'y' | pecl install memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
-else
-    wget "http://pecl.php.net/get/memcache-$VERSION.tgz"
-    tar -zxf "memcache-$VERSION.tgz"
-    sh -c "cd memcache-$VERSION && phpize && ./configure --enable-memcache && make && sudo make install"
-    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
-fi
+    return $?
+}
+
+install_memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
