88e4de00abf63c7a198fbd12f595021be623e443
Kamil Dziedzic <arvenil@klecza.pl>
2013-02-18 21:20:36 +0100

    indent

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -3,10 +3,10 @@
 VERSION="2.2.7"
 
 if [[ "$TRAVIS_PHP_VERSION" < "5.5" ]]; then
-echo 'y' | pecl install memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
+    echo 'y' | pecl install memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
 else
-wget "http://pecl.php.net/get/memcache-$VERSION.tgz"
-  tar -zxf "memcache-$VERSION.tgz"
-  sh -c "cd memcache-$VERSION && phpize && ./configure --enable-memcache && make && sudo make install"
-  echo "memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+    wget "http://pecl.php.net/get/memcache-$VERSION.tgz"
+    tar -zxf "memcache-$VERSION.tgz"
+    sh -c "cd memcache-$VERSION && phpize && ./configure --enable-memcache && make && sudo make install"
+    echo "memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
 fi
