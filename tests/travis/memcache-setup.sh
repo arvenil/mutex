dd8d479bc5cd65d4eff336f80f0fd235e7e8d9c8
Kamil Dziedzic <arvenil@klecza.pl>
2013-02-20 23:31:08 +0100

    Proper use of variables inside ""

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 install_memcache() {
-    if [ $(expr "$TRAVIS_PHP_VERSION" "<" "5.5") -eq 1 ]; then
+    if [ $(expr "${TRAVIS_PHP_VERSION}" "<" "5.5") -eq 1 ]; then
         echo 'y' | pecl install memcache
     else
         MEMCACHE_VERSION="2.2.7"
-        wget "http://pecl.php.net/get/memcache-$MEMCACHE_VERSION.tgz" &&
-        tar -zxf "memcache-$MEMCACHE_VERSION.tgz" &&
-        sh -c "cd memcache-$MEMCACHE_VERSION && phpize && ./configure --enable-memcache && make && sudo make install" &&
+        wget "http://pecl.php.net/get/memcache-${MEMCACHE_VERSION}.tgz" &&
+        tar -zxf "memcache-${MEMCACHE_VERSION}.tgz" &&
+        sh -c "cd memcache-${MEMCACHE_VERSION} && phpize && ./configure --enable-memcache && make && sudo make install" &&
         echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
     fi
 
