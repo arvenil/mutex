eb8b4d8ae16d0d0de5daee74352635c493f2bb02
Kamil Dziedzic <arvenil@klecza.pl>
2013-03-24 23:53:06 +0100

    Install new libmemcached for php 5.5 build

--- tests/travis/memcached-setup.sh
+++ tests/travis/memcached-setup.sh
@@ -2,6 +2,12 @@
 
 install_memcached() {
     if [ $(expr "${TRAVIS_PHP_VERSION}" ">=" "5.5") -eq 1 ]; then
+        LIBMEMCACHED_VERSION="1.0.13"
+        sudo apt-get install libevent-dev libcloog-ppl0
+        wget "https://launchpad.net/libmemcached/1.0/${LIBMEMCACHED_VERSION}/+download/libmemcached-${LIBMEMCACHED_VERSION}.tar.gz" &&
+        tar xzf "libmemcached-${LIBMEMCACHED_VERSION}.tar.gz" &&
+        sh -c "cd libmemcached-${LIBMEMCACHED_VERSION} && ./configure && make && sudo make install"
+
         MEMCACHED_VERSION="2.1.0"
         wget "http://pecl.php.net/get/memcached-${MEMCACHED_VERSION}.tgz" &&
         tar -zxf "memcached-${MEMCACHED_VERSION}.tgz" &&
