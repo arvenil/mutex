4c8801b8bcbeff8509399c75a16c0fcc10293fe7
Kamil Dziedzic <arvenil@klecza.pl>
2013-03-25 00:02:16 +0100

    bugfix: broken build should now gracefully fail

--- tests/travis/memcached-setup.sh
+++ tests/travis/memcached-setup.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 install_memcached() {
+    echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+
     if [ $(expr "${TRAVIS_PHP_VERSION}" ">=" "5.5") -eq 1 ]; then
+        MEMCACHED_VERSION="2.1.0"
         LIBMEMCACHED_VERSION="1.0.13"
-        sudo apt-get install libevent-dev libcloog-ppl0
+        sudo apt-get install libevent-dev libcloog-ppl0 &&
         wget "https://launchpad.net/libmemcached/1.0/${LIBMEMCACHED_VERSION}/+download/libmemcached-${LIBMEMCACHED_VERSION}.tar.gz" &&
         tar xzf "libmemcached-${LIBMEMCACHED_VERSION}.tar.gz" &&
-        sh -c "cd libmemcached-${LIBMEMCACHED_VERSION} && ./configure && make && sudo make install"
-
-        MEMCACHED_VERSION="2.1.0"
+        sh -c "cd libmemcached-${LIBMEMCACHED_VERSION} && ./configure && make && sudo make install" &&
         wget "http://pecl.php.net/get/memcached-${MEMCACHED_VERSION}.tgz" &&
         tar -zxf "memcached-${MEMCACHED_VERSION}.tgz" &&
         sh -c "cd memcached-${MEMCACHED_VERSION} && phpize && ./configure --enable-memcached && make && sudo make install"
     fi
-
-    echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
 
     return $?
 }
