9617c6174658b3d7692f5475a072c2690b7b3d32
Kamil Dziedzic <arvenil@klecza.pl>
2013-03-24 22:53:46 +0100

    Enable memcached extension in travis

---
+++ tests/travis/memcached-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+install_memcache() {
+    if [ $(expr "${TRAVIS_PHP_VERSION}" ">=" "5.5") -eq 1 ]; then
+        MEMCACHED_VERSION="2.1.0"
+        wget "http://pecl.php.net/get/memcached-${MEMCACHED_VERSION}.tgz" &&
+        tar -zxf "memcached-${MEMCACHED_VERSION}.tgz" &&
+        sh -c "cd memcached-${MEMCACHED_VERSION} && phpize && ./configure --enable-memcached && make && sudo make install"
+    fi
+
+    echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+
+    return $?
+}
+
+install_memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
