1d7829265ab0d5ab33dc752f50ef560f0bf39046
Kamil Dziedzic <arvenil@klecza.pl>
2013-03-25 00:55:54 +0100

    bugfix: broken build should now gracefully fail

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 install_memcache() {
+    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+
     if [ $(expr "${TRAVIS_PHP_VERSION}" ">=" "5.5") -eq 1 ]; then
         MEMCACHE_VERSION="2.2.7"
         wget "http://pecl.php.net/get/memcache-${MEMCACHE_VERSION}.tgz" &&
         tar -zxf "memcache-${MEMCACHE_VERSION}.tgz" &&
         sh -c "cd memcache-${MEMCACHE_VERSION} && phpize && ./configure --enable-memcache && make && sudo make install"
     fi
-
-    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
 
     return $?
 }
